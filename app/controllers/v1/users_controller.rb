32b47c67abe43a8e0f46c85a7b9ea94921d76894
vzdrizhni <vzdrizhni@gmail.com>
2020-09-14 19:08:39 +0500

    Add user registration

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,18 @@
+module V1
+    class UsersController < ApplicationController
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                render :create
+            else
+                head(:unprocessable_entity)
+            end
+        end
+
+        private
+
+        def user_params
+            params.require(:user).permit(:email, :password)
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  scope :api, defaults: { format: :json } do
-    devise_for :users
-  end
+  # scope :api, defaults: { format: :json } do
+  #   devise_for :users
+  # end
 
   namespace :v1, defaults: { format: :json } do
     resources :contacts
     resources :sessions, only: [:create, :destroy]
+    resources :users, only: [:create, :destroy]
   end
 end
